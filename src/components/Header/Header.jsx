97ab6944316a2f28190ece1b7a871458b889543e
Niall Kenny <NiallKenny@Nialls-MacBook-Pro.local>
2019-08-12 10:35:37 +0100

    centered nc-news btn on header.Added _redirects file for netlify.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,17 +6,19 @@ const Header = ({ loggedIN }) => {
   return (
     <div className="header">
       <nav className="nav-bar">
-        <div className="navigation">
+        <div>
           <Link to="/" className="option">
             HOME
           </Link>
           <Link to="/feed" className="option">
             FEED
           </Link>
-          <Link to="/" className="nc-news-btn">
-            NC-News
-          </Link>
         </div>
+
+        <Link to="/" className="nc-news-btn">
+          NC-News
+        </Link>
+
         <div className="loggedIN">
           <Link to={`/user/${loggedIN}`} className="option">
             {loggedIN}
