b05e6f3878c27e9725b945ae2049cfb4ab0d46b0
Niall Kenny <NiallKenny@Nialls-MacBook-Pro.local>
2019-08-15 21:09:24 +0100

    added styling to header and feed page buttons (turn grey on hover).

--- src/components/SortBy/SortBy-component.jsx
+++ src/components/SortBy/SortBy-component.jsx
@@ -1,36 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import "./sortBy.styles.scss";
 
-class SortBy extends Component {
-  render() {
-    return (
-      <div className="sort-by">
-        <button
-          name="created_at"
-          className="sort-by-btn"
-          onClick={this.handleClick}
-        >
-          date created
-        </button>
-        <button
-          name="comment_count"
-          className="sort-by-btn-m"
-          onClick={this.handleClick}
-        >
-          comments count
-        </button>
-        <button name="votes" className="sort-by-btn" onClick={this.handleClick}>
-          votes
-        </button>
-      </div>
-    );
-  }
-  handleClick = e => {
-    //sends name to query data
-    const { name } = e.target;
-    const { updateSortOrder } = this.props;
-    updateSortOrder(name);
-  };
-}
+const SortBy = ({ updateSortOrder }) => {
+  return (
+    <div className="sort-by">
+      <button
+        name="created_at"
+        className="sort-by-btn"
+        onClick={({ target }) => updateSortOrder(target.name)}
+      >
+        date created
+      </button>
+      <button
+        name="comment_count"
+        className="sort-by-btn-m"
+        onClick={({ target }) => updateSortOrder(target.name)}
+      >
+        comments count
+      </button>
+      <button
+        name="votes"
+        className="sort-by-btn"
+        onClick={({ target }) => updateSortOrder(target.name)}
+      >
+        votes
+      </button>
+    </div>
+  );
+};
 
 export default SortBy;
